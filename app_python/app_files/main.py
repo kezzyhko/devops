3c728688d0961951801daa4b0f107dcb2dfcf8f2
Sergey Semushin <kezzyhko@gmail.com>
2021-08-21 12:41:16 +0300

    Initial commit

---
+++ app_python/app_files/main.py
@@ -0,0 +1,29 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
+import pytz
+
+
+
+ADDR = ''
+PORT = 5000
+CHARSET = 'utf-8'
+DATETIME_TIMEZONE = pytz.timezone('Europe/Moscow')
+DATETIME_FORMAT = "%d/%m/%Y %H:%M:%S"
+
+
+
+class XXX(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html; charset=' + CHARSET)
+		self.end_headers()
+
+		datetime_to_print = datetime.now(DATETIME_TIMEZONE).strftime(DATETIME_FORMAT)
+		self.wfile.write(datetime_to_print.encode(CHARSET))
+		
+
+
+
+if __name__ == '__main__':
+	server = HTTPServer((ADDR, PORT), XXX)
+	server.serve_forever()
